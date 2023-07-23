4065e8ddf0d6cd74b43161abb991e9e6e03744a4
bradccb <125482656@qq.com>
2023-07-24 00:12:38 +0800

    first commit

---
+++ trainbaichuan.sh
@@ -0,0 +1,19 @@
+CUDA_VISIBLE_DEVICES=0 python src/train_sft.py \
+    --model_name_or_path /work/LLM/baichuan/baichuan-7B/baichuan-inc/baichuan-7B \
+    --do_train \
+    --auto_find_batch_size True \
+    --dataset alpaca_zh \
+    --finetuning_type lora \
+    --lora_target W_pack \
+    --output_dir path_to_sft_checkpoint \
+    --overwrite_cache \
+    --per_device_train_batch_size 4 \
+    --gradient_accumulation_steps 4 \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --save_steps 1000 \
+    --learning_rate 5e-5 \
+    --num_train_epochs 1 \
+    --plot_loss \
+    --fp16 \
+    --baichuan_rtx_gpu true
